1cd4a6b15d49cc699e2206a906116ffcf78a22af
harsimran498 <35481490+harsimran498@users.noreply.github.com>
2019-03-12 19:15:57 +0530

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/sh
 ##Remove all containers using the employee image##
-docker rm $(docker ps -a | grep employee-microservice-nodenpminstall | awk '{print $1}')
+rem = "docker ps -a | grep employee-microservice-nodenpminstall | awk '{print $1}'"
+docker rm $rem
