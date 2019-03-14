b4ffacb61222eedfdff7cbdbe13f3374c73c0527
harsimran498 <35481490+harsimran498@users.noreply.github.com>
2019-03-14 15:38:02 +0530

    Update kill-8002.sh

--- kill-8002.sh
+++ kill-8002.sh
@@ -1,2 +1,9 @@
-netstat -ltnp | grep -w ':8002' | awk -F "LISTEN" '{print $2}'|  awk -F "/" '{print $1}' |  xargs kill -9
-exit 0 
+#!/usr/bin/sh
+netstat -ltnp | grep -w ':8002' | awk -F "LISTEN" '{print $2}'|  awk -F "/" '{print $1}' |  xargs kill -9 &> /dev/null
+if [ $? -eq 0 ]
+then
+  echo " Process killed running on port 8002 already"
+else
+  echo " Process not running on port port 8002 "
+  exit 0
+fi
