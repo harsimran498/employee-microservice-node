258b981575e727d8f97e47f9772ffcadddacb9fe
harsimran498 <35481490+harsimran498@users.noreply.github.com>
2019-03-14 15:52:59 +0530

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -3,3 +3,10 @@
 docker stop `docker ps -a | grep 8002 | awk '{print $1}'`
 docker rm `docker ps -a | grep 8002 | awk '{print $1}'`
 
+if [ $? -eq 0 ]
+then
+  echo " Process killed running on port 8002 already"
+else
+  echo " Process not running on port port 8002 "
+  exit 0
+fi
