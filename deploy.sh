2f58f24edb353f74ae24b44945fca93f23fbf2b5
harsimran498 <35481490+harsimran498@users.noreply.github.com>
2019-03-17 01:09:10 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 environment=$1
 BUILDNO=$2
 
-ansible-playbook -i hosts docker.yml --limit $environment -e "build=DEV.29"
+ansible-playbook -i hosts sample.yml --limit $environment -e "build=DEV.29"
