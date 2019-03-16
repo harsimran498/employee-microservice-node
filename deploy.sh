03913c027a7bacad97880a2852e802eb88f8de18
harsimran498 <35481490+harsimran498@users.noreply.github.com>
2019-03-17 00:27:39 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/sh
 
 environment=$1
+BUILDNO=$2
 
-ansible-playbook -i hosts docker.yml --limit $environment
+ansible-playbook -i hosts docker.yml --limit $environment --extra-vars "BUILD=$BUILDNO"
