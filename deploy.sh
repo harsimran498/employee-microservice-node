cca572975e8e45eb73bdb73f36f9e2eb6ffeb9f4
harsimran498 <35481490+harsimran498@users.noreply.github.com>
2019-03-17 01:18:30 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,6 @@
 environment=$1
 BUILDNO=$2
 
-ansible-playbook -i hosts sample.yml --limit $environment -e "build=Dev.82"
+#ansible-playbook -i hosts sample.yml --limit $environment -e "build=Dev.82"
+
+ansible-playbook -i hosts sample.yml --limit $environment -e "build=$BUILDNO"
