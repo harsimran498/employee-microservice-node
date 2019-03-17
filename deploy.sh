cca1718b3670cc147d0c14d9d92de55e0c046f1e
harsimran498 <35481490+harsimran498@users.noreply.github.com>
2019-03-17 21:10:45 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,7 @@
 
 environment=$1
 BUILDNO=$2
+user=$3
+password=$4
 
-ansible-playbook -i hosts sample.yml --limit $environment -e "build=$BUILDNO"
+ansible-playbook -i hosts sample.yml --limit $environment -e "build=$BUILDNO user=$user password=$password"
