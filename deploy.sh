5198377bd315de4df6c6fb3c46d4a8abe938959f
harsimran498 <35481490+harsimran498@users.noreply.github.com>
2019-03-25 20:03:13 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,8 @@
 #BUILDNUMBER=`echo $BUILDNO | awk -F "PACKAGE/" '{print $2}' | awk -F "/" '{print $1}'`
 #BUILD=DEPLOY.$BUILDNUMBER
 
+ENVIRONMENT=$2
 
 BUILDNUMBER=`echo $1 | awk -F "PACKAGE/" '{print $2}' | awk -F "/" '{print $1}'`
-BUILD=DEPLOY.$2
+BUILD=DEPLOY.$BUILDNUMBER
 ansible-playbook -i hosts docker.yml --limit $ENVIRONMENT -e "build=$BUILD user=admin password=admin123"
