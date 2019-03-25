81b04c84d00f93811584469c3ac6f2bd2f7c2772
harsimran498 <35481490+harsimran498@users.noreply.github.com>
2019-03-25 20:11:49 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,8 @@
 #BUILD=DEPLOY.$BUILDNUMBER
 
 ENVIRONMENT=$2
+NEXUSPWD=$3
 
 BUILDNUMBER=`echo $1 | awk -F "Microservice/" '{print $2}' | awk -F "/" '{print $1}'`
 BUILD=DEPLOY.$BUILDNUMBER
-ansible-playbook -i hosts docker.yml --limit $ENVIRONMENT -e "build=$BUILD user=admin password=admin123"
+ansible-playbook -i hosts docker.yml --limit $ENVIRONMENT -e "build=$BUILD user=admin password=$NEXUSPWD"
